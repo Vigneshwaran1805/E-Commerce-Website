df008fbffc8014676c4e37b145a5eb5851865c5f
Vigneshwaran1805 <vigneshsaravanan1805@gmail.com>
2025-05-27 19:37:56 +0530

    Delete routes directory

--- routes/product.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const { getProducts, getSingleProduct1 } = require('../controllers/productController');
-const router=express.Router();
-
-router.route('/products').get(getProducts);
-router.route('/product/:id').get(getSingleProduct1);
-
-module.exports =router;
